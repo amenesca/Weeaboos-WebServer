794389c9a04d5e5e3d476c8757f749073437421b
amenesca <alanmenescal@hotmail.com>
2023-12-04 21:24:23 -0300

    adicionado arquivos para teste e estudos

---
+++ estudos_servertcp/common.h
@@ -0,0 +1,27 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+
+#define SERVER_PORT 18000
+
+#define MAXLINE 4096
+#define SA struct sockaddr
+
+void err_n_die(const char *fmt, ...);
+char *bin2hex(const unsigned char *input, size_t len);
+
+#endif
