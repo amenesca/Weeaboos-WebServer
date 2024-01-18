cc49e93135dd6f610c55ae70d21a5bafe7a8c8d2
Alan Menescal Cáceres <66499569+amenesca@users.noreply.github.com>
2024-01-18 14:08:51 +0000

    Setters e getters da classe client feitos

--- srcs/Client/Client.cpp
+++ srcs/Client/Client.cpp
@@ -2,28 +2,44 @@
 
 Client::Client() {
     _requestBuffer = new u_int8_t[MAX_BUFFER_SIZE + 1];
+    _client_addr_len = sizeof(_client_addr);
 }
 
 Client::~Client() {
-    delete[] _requestBuffer;
+    if (_requestBuffer != NULL) {
+        delete _requestBuffer;
+        _requestBuffer = NULL;
+    }
 }
 
 u_int8_t **Client::getBufferAddress(void) {
     return (&_requestBuffer);
 }
 
+SA *Client::getClientAddrPointer(void) {
+    return &_client_addr;
+}
+
+socklen_t *Client::getClientAddrLenPointer(void) {
+    return &_client_addr_len;
+}
+
 int Client::getClientSocket(void) const {
     return _clientSocket;
 }
 
-socklen_t Client::getClientAddrLen(void) const {
-    return _client_addr_len;
-}
-
-SA Client::getClientAddr(void) const {
-    return _client_addr;
-}
-
 ssize_t Client::getBytesRead(void) const {
     return _bytesRead;
+}
+
+void Client::setClientSocket(int clientSocket) {
+    _clientSocket = clientSocket;
+}
+
+void Client::setClientAddrLen(socklen_t client_addr_len) {
+    _client_addr_len = client_addr_len;
+}
+
+void Client::setBytesRead(ssize_t bytesRead) {
+    _bytesRead = bytesRead;
 }
--- srcs/Client/Client.hpp
+++ srcs/Client/Client.hpp
@@ -5,16 +5,25 @@
 # include "../../includes/Includes.hpp"
 # include "../../includes/Defines.hpp"
 
+// Classe Client responsável por guardar informações do cliente conectado,
+// dentro do Servidor será um vector que manterá atualizado todos os clientes
+// Foi necessário fazer vários getters e setters para funcionar além de
+// destructors e constructors personalizados.
+
 class Client {
     public:
         Client();
         ~Client();
 
         u_int8_t    **getBufferAddress(void);
-        int         getClientSocket() const;
-        socklen_t   getClientAddrLen() const;
-        SA          getClientAddr() const;
-        ssize_t     getBytesRead() const;
+        SA          *getClientAddrPointer(void);
+        socklen_t   *getClientAddrLenPointer(void);
+        int         getClientSocket(void) const;
+        ssize_t     getBytesRead(void) const;
+
+        void        setClientSocket(int clientSocket);
+        void        setClientAddrLen(socklen_t _client_addr_len);
+        void        setBytesRead(ssize_t bytesRead);
     private:
         int			_clientSocket;
         socklen_t   _client_addr_len;
