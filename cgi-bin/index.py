138475ff71cd8b94f9cb5e98f84e98fab4ed4763
nands93 <femmarques93+github@gmail.com>
2024-02-26 18:33:20 -0300

    testando POST method

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import cgi
-print("HTTP/1.1 200 OK\r\n")
-print("<!DOCTYPE html>")
-print("<html>")
-print("<body>")
-print("<h1>Hello, world!</h1>")
-print("</body>")
-print("</html>")
+import os
+
+# Cabeçalhos MIME
+print("Content-Type: text/plain")  # Indica o tipo de conteúdo como texto simples
+print()  # Imprime uma linha em branco para indicar o fim dos cabeçalhos
+
+# Receber dados do formulário
+form = cgi.FieldStorage()
+
+nome = form.getvalue("nome")
+email = form.getvalue("email")
+imagem = form["imagem"]
+
+if imagem.filename:  # Verifica se um arquivo foi selecionado
+    # Define o caminho para onde a imagem será movida
+    destino = os.path.join("../enviados", imagem.filename)
+
+    # Move a imagem para o destino
+    with open(destino, "wb") as f:
+        f.write(imagem.file.read())
+
+    # Retorna o caminho da imagem enviada
+    print(destino)
+else:
+    # Retorna uma mensagem de erro se nenhum arquivo foi selecionado
+    print("Nenhuma imagem selecionada para enviar.")
